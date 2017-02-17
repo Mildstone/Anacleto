816f1b80351a4f2cbb1b9ea95b43950a1bffc5f4
zack-vii <zack-vii@live.com>
2017-02-17 15:56:43 +0100

    finally understood axi valid-ready relation
    out_ready until in_valid
    in_valid  until out_ready again
    
    ready """\__/"""
    valid __/""""\__

--- projects/w7x_timing/logic/w7x_timing_1.0/hdl/w7x_timing_v1_0_S00_AXI.vhd
+++ projects/w7x_timing/logic/w7x_timing_1.0/hdl/w7x_timing_v1_0_S00_AXI.vhd
@@ -48,11 +48,12 @@ architecture arch_imp of w7x_timing_v1_0_S00_AXI is
     constant DATA_LIMIT : unsigned(ADDR_WIDTH-1 downto 0) := to_unsigned(DATA_COUNT,ADDR_WIDTH);
     -- AXI4LITE signals
     signal axi_aaddr   : unsigned(ADDR_WIDTH-1 downto 0);
-    signal axi_wready  : std_logic := '0';
+    signal axi_awready : std_logic := '1';
+    signal axi_wready  : std_logic := '1';
     signal axi_wdata   : std_logic_vector(DATA_WIDTH-1 downto 0);
     signal axi_wstrb   : std_logic_vector(DATA_WIDTH/8-1 downto 0);
     signal axi_bvalid  : std_logic := '0';
-    signal axi_arready : std_logic := '0';
+    signal axi_arready : std_logic := '1';
     signal axi_rdata   : std_logic_vector(DATA_WIDTH-1 downto 0);
     signal axi_rvalid  : std_logic := '0';
     signal bram_en     : std_logic := '0';
@@ -72,12 +73,12 @@ begin
     ADDR_OUT <= unsigned(axi_aaddr);
     DATA_OUT <= axi_wdata;
     STRB_OUT <= axi_wstrb;
-    EN_OUT   <= (S_AXI_AWVALID and S_AXI_WVALID) or S_AXI_ARVALID or bram_en;
+    EN_OUT   <= S_AXI_AWVALID or S_AXI_ARVALID or bram_en;
     RST_OUT  <= not S_AXI_RESETN;
-    WE_OUT   <= axi_wready;
+    WE_OUT   <= not axi_wready;
 ---- AXI interface
     -- I/O Connections assignments
-    S_AXI_AWREADY <= axi_wready;
+    S_AXI_AWREADY <= axi_awready;
     S_AXI_WREADY  <= axi_wready;
     S_AXI_BRESP   <= "00";
     S_AXI_BVALID  <= axi_bvalid;
@@ -85,80 +86,93 @@ begin
     S_AXI_RDATA   <= axi_rdata;
     S_AXI_RRESP   <= "00";
     S_AXI_RVALID  <= axi_rvalid;
-    axi_rdata  <= DATA_IN;
-
 
     process (S_AXI_CLK,S_AXI_RESETN,
              S_AXI_AWVALID,S_AXI_WVALID,S_AXI_AWADDR,S_AXI_WSTRB,S_AXI_WDATA, S_AXI_BREADY,
              S_AXI_ARADDR, S_AXI_RREADY,
              DATA_IN)
 
-    procedure wset0 is begin
-      axi_wready  <= '0';
-      axi_bvalid  <= '0';
-      axi_wdata   <= (others => '0');
-      axi_wstrb   <= (others => '0');
-      if (axi_arready or S_AXI_RESETN) = '0' then
+    procedure check is begin
+      if (S_AXI_RESETN and not axi_wready) = '0' then
+        axi_wdata   <= (others => '0');
+        axi_wstrb   <= (others => '0');
+      end if;
+      if (S_AXI_RESETN and axi_rvalid) = '0' then
+        axi_rdata   <= (others => '0');
+      end if;
+      if (S_AXI_RESETN and not (axi_awready and axi_arready)) = '0' then
         axi_aaddr   <= (others => '1');
         bram_en     <= '0';
       end if;
-    end wset0;
-    procedure wset1 is begin
-      bram_en     <= '1';
+    end check;
+
+-- ready """"""\____/""""
+-- valid _____/""""""\___
+
+    procedure wgo_idle0 is begin
+      axi_awready <= '1';
       axi_wready  <= '1';
+      axi_bvalid  <= '0';
+    end wgo_idle0;
+    procedure wgo_awready1 is begin
+      bram_en     <= '1';
+      axi_awready <= '0';
       axi_aaddr   <= axi_addr2addr(S_AXI_AWADDR);
+    end wgo_awready1;
+    procedure wgo_wready2 is begin
+      axi_wready  <= '0';
       axi_wstrb   <= S_AXI_WSTRB;
       axi_wdata   <= S_AXI_WDATA;
-    end wset1;
-    procedure wset2 is begin
+    end wgo_wready2;
+    procedure wgo_bvalid3 is begin
       axi_bvalid <= '1';
-    end wset2;
+    end wgo_bvalid3;
 
-    procedure rset0 is begin
-      axi_arready <= '0';
-      axi_rvalid  <= '0';
-      --axi_rdata   <= (others => '0');
-      if (axi_wready or S_AXI_RESETN) = '0' then
-        axi_aaddr   <= (others => '1');
-        bram_en     <= '0';
-      end if;
-    end rset0;
-    procedure rset1 is begin
-      bram_en     <= '1';
+    procedure rgo_idle0 is begin
       axi_arready <= '1';
+      axi_rvalid  <= '0';
+    end rgo_idle0;
+    procedure rgo_arready1 is begin
+      bram_en     <= '1';
+      axi_arready <= '0';
       axi_aaddr   <= axi_addr2addr(S_AXI_ARADDR);
-    end rset1;
-    procedure rset2 is begin
-      axi_rvalid <= '1';
-    end rset2;
+    end rgo_arready1;
+    procedure rgo_rvalid2 is begin
+      axi_arready <= '1';
+      axi_rvalid  <= '1';
+      axi_rdata  <= DATA_IN;
+    end rgo_rvalid2;
 
     begin
       if rising_edge(S_AXI_CLK) then
+        check;
         if S_AXI_RESETN = '0' then
-          wset0;
-          rset0;
-        elsif (axi_wready or (S_AXI_AWVALID and S_AXI_WVALID and not axi_arready)) = '1'  then
+          wgo_idle0;
+          rgo_idle0;
+        -- writing
+        elsif axi_awready = '0' then
           if axi_wready = '0' then
-            rset0;
-            wset1;
-          else
             if axi_bvalid = '0' then
-              wset2;
+              wgo_bvalid3;
             elsif S_AXI_BREADY = '1' then
-              wset0;
+              wgo_idle0;
             end if;
+          elsif S_AXI_WVALID = '1' then
+            wgo_wready2;
           end if;
-        elsif (axi_arready or (S_AXI_ARVALID and not axi_wready)) = '1' then
-          if axi_arready = '0' then
-            wset0;
-            rset1;
-          else
-            if axi_rvalid = '0' then
-              rset2;
-            elsif S_AXI_RREADY  = '1' then
-              rset0;              
-            end if;
-          end if;
+        elsif (S_AXI_AWVALID and axi_arready) = '1'  then
+          rgo_idle0;
+          wgo_awready1;
+        -- reading
+        elsif axi_arready = '0' then
+          if axi_rvalid = '0' then
+            rgo_rvalid2;
+          elsif S_AXI_RREADY  = '1' then
+            rgo_idle0;              
+          end if;        
+        elsif (S_AXI_ARVALID and axi_awready) = '1' then
+          wgo_idle0;
+          rgo_arready1;
         end if;
       end if;
     end process;
