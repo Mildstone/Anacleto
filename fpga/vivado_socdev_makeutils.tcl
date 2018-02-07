7fbb13d71507d7e9aa53bec50ba5d719671e481c
AndreaRigoni <andrea.rigoni@pd.infn.it>
2018-02-07 13:53:10 +0100

    add gpl license

--- fpga/vivado_socdev_makeutils.tcl
+++ fpga/vivado_socdev_makeutils.tcl
@@ -1,3 +1,24 @@
+# ////////////////////////////////////////////////////////////////////////// //
+#
+# This file is part of the anacleto project.
+# Copyright 2018 Andrea Rigoni Garola <andrea.rigoni@igi.cnr.it>.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# ////////////////////////////////////////////////////////////////////////// //
+
+
 package provide makeutils 1.0
 # package require Vivado 1.2014.1
 
@@ -24,7 +45,6 @@ namespace eval ::tclapp::socdev::makeutils {
 catch {
   source -notrace $top_srcdir/fpga/vivado_socdev_env.tcl
   source -notrace $top_srcdir/fpga/vivado_socdev_listutils.tcl
-  source -notrace $top_srcdir/fpga/write_project_tcl_devel.tcl
   source -notrace $top_srcdir/fpga/write_anacleto_prj.tcl
 }
 
