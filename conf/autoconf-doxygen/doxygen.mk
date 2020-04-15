97605273fbe760e4afe67d697c45818057e3bcbf
Andrea Rigoni <andrea.rgn@gmail.com>
2020-04-15 12:52:22 +0200

    fix: init doxygen

--- conf/autoconf-doxygen/doxygen.mk
+++ conf/autoconf-doxygen/doxygen.mk
@@ -71,10 +71,15 @@ $(DX_DESTDIR)/latex/refman.pdf: $(DX_DESTDIR)/latex
 ${DX_DESTDIR}/${DX_PACKAGE_NAME}.pdf: $(DX_DESTDIR)/latex/refman.pdf
 	cp $< $@
 
+
+doxygen-update: ##@docs update latex config file
+	$(DOXYGEN_BINARY) -u $(DX_CFG);
+
+
 html:  ##@docs generate html documentation
 html:  $(DX_DESTDIR)/html
 latex: ##@docs generate latex documentation
-latex: $(DX_DESTDIR)/latex
+latex: doxygen-update $(DX_DESTDIR)/latex
 pdf:   ##@docs generate pdf manual
 pdf:   $(DX_DESTDIR)/${DX_PACKAGE_NAME}.pdf
 
